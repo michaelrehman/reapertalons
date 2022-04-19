e6429d605ddf5c7d6c6b07aaf05f10a809e8f55e
Michael Rehman <michael@michaelrehman.com>
2022-04-18 23:56:56 -0400

    Working buff

--- ReaperTalons/ReaperTalons.cs
+++ ReaperTalons/ReaperTalons.cs
@@ -52,15 +52,16 @@ namespace ReaperTalons {
             itemDefinition.pickupModelPrefab = Resources.Load<GameObject>("Prefabs/PickupModels/PickupMystery");
 
             var displayRules = new ItemDisplayRuleDict(null);
-            ItemAPI.Add(new CustomItem(itemDefinition, displayRules)); // add to game
+            R2API.ItemAPI.Add(new CustomItem(itemDefinition, displayRules)); // add to game
         } // DefineItem
 
         /// <summary>
         /// Sets the neccessary hooks needed for this item to function.
         /// </summary>
         private void SetHooks() {
-            On.RoR2.HealthComponent.TakeDamage += HealthComponent_TakeDamage;
-            On.RoR2.HealthComponent.Heal += HealthComponent_Heal;
+            // On.RoR2.HealthComponent.TakeDamage += HealthComponent_TakeDamage;
+            // On.RoR2.HealthComponent.Heal += HealthComponent_Heal;
+            R2API.RecalculateStatsAPI.GetStatCoefficients += RecalculateStatsAPI_GetStatCoefficients;
         } // SetHooks
 
         private float HealthComponent_Heal(On.RoR2.HealthComponent.orig_Heal orig, HealthComponent self, float amount, ProcChainMask procChainMask, bool nonRegen) {
@@ -78,42 +79,44 @@ namespace ReaperTalons {
         /// </summary>
         /// <param name="healthComponent">The entity to apply the buff to.</param>
         private void ManageBuff(HealthComponent healthComponent) {
-            Log.LogDebug(healthComponent.health + " / " + healthComponent.body.maxHealth + " = " + healthComponent.health / healthComponent.body.maxHealth * 100 + "% of HP remaining for " + healthComponent.body.name);
+            Log.LogDebug(nameof(ManageBuff) + " called.");
+            RecalculateStatsAPI_GetStatCoefficients(healthComponent.body, new RecalculateStatsAPI.StatHookEventArgs()); // won't actually update stats :(
+        } // ManageBuff
 
-            CharacterBody body = healthComponent.body;
-            Inventory inventory = body?.inventory;
-            if (!inventory) {
-                return;
-            } // if
-
-            int itemCount = inventory.GetItemCount(itemDefinition.itemIndex);
+        private void RecalculateStatsAPI_GetStatCoefficients(CharacterBody sender, RecalculateStatsAPI.StatHookEventArgs args) {
+            int itemCount = sender?.inventory.GetItemCount(itemDefinition.itemIndex) ?? 0;
             if (itemCount <= 0) {
                 return;
             } // if
 
-            // Caclulate the buff amount
-            float hpMissingPercent = (1 - healthComponent.health / healthComponent.body.maxHealth) * 100;
-            float buffPercent = BUFF_COEFFICIENT * itemCount * hpMissingPercent;
+            float hpMissingPercent = (1 - sender.healthComponent.health / sender.healthComponent.body.maxHealth) * 100;
+            float buffMultiplier = BUFF_COEFFICIENT * itemCount * hpMissingPercent / 100;
 
-            // TODO: apply the buff
-
-        } // ManageBuff
+            args.damageMultAdd += buffMultiplier;
+            args.attackSpeedMultAdd += buffMultiplier;
+            args.moveSpeedMultAdd += buffMultiplier;
+            Log.LogDebug("RECALC CALLED for " + sender.name + ". Missing " + hpMissingPercent + "% of HP resulting in a " + buffMultiplier + " buff. Current damage is: " + sender.damage);
+        } // RecalculateStatsAPI_GetStatCoefficients
 
         private void AddTokens() {
             LanguageAPI.Add("ITEM_BUFFWHENLOW_NAME", "Reaper Talons");
             LanguageAPI.Add("ITEM_BUFFWHENLOW_PICKUP", "Grow stronger as death approaches... <color=#ff7f7f>BUT take continuous damage.</color>");
-            LanguageAPI.Add("ITEM_BUFFWHENLOW_DESC", "Convert each percent of missing HP to a <style=cIsDamage>" + BUFF_COEFFICIENT + "%</style> <style=cStack>(+" + BUFF_COEFFICIENT + "% per stack)</style> increase to <style=cIsDamage>base damage</style>, <style=cIsDamage>attack speed</style>, and <style=cIsUtility>movement speed</style>, but <style=cIsHealing>lose" + DAMAGE_COEFFICIENT + "% <style=cStack>(+" + DAMAGE_COEFFICIENT + "% per stack)</style> of current HP every second</style>.");
+            LanguageAPI.Add("ITEM_BUFFWHENLOW_DESC", "Convert each percent of missing HP to a <style=cIsDamage>" + BUFF_COEFFICIENT + "%</style> <style=cStack>(+" + BUFF_COEFFICIENT + "% per stack)</style> increase to <style=cIsDamage>base damage</style>, <style=cIsDamage>attack speed</style>, and <style=cIsUtility>movement speed</style>, but <style=cIsHealing>lose " + DAMAGE_COEFFICIENT + "% <style=cStack>(+" + DAMAGE_COEFFICIENT + "% per stack)</style> of current HP every second</style>.");
             LanguageAPI.Add("ITEM_BUFFWHENLOW_LORE", "UNDER CONSTRUCTION!!!");
         } // AddTokens
 
         private void Update() {
             if (Input.GetKeyDown(KeyCode.P)) {
-                // Get the player body to use a position:
                 var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
-
-                // And then drop our defined item in front of the player.
-                Log.LogInfo($"Player pressed F2. Spawning our custom item at coordinates {transform.position}");
                 PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(itemDefinition.itemIndex), transform.position, transform.forward * 20f);
+            } // if
+
+            if (Input.GetKeyDown(KeyCode.O)) {
+                var transform = PlayerCharacterMasterController.instances[0].master;
+                DamageInfo di = new() {
+                    damage = transform.GetBody().healthComponent.health / 2
+                };
+                transform.GetBody().healthComponent.TakeDamage(di);
             } // if
         } // Update
     } // ReaperTalons
